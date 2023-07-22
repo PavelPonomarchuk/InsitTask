6334f9c47f143ba65c2ac447480ac9a6bc6825ea
Pavel Ponomarchuk <ponomarchuk83@gmail.com>
2023-07-22 18:34:41 +0500

    Added extensions file

---
+++ app/src/main/java/ru/ponomarchukpn/insittask/utils/Extensions.kt
@@ -0,0 +1,8 @@
+package ru.ponomarchukpn.insittask.utils
+
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+fun Fragment.showToast(message: String) {
+    Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+}
